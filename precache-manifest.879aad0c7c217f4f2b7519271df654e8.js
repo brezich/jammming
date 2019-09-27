5c1d29571947e07775c71708c32d15cb30ae20ad
brezich <brezich@yahoo.com>
2019-09-27 19:48:30 +0200

    Updates

---
+++ precache-manifest.879aad0c7c217f4f2b7519271df654e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e400c7417620b09f9e84835942c5cb4",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "cf219bc3143fda19a3b9",
+    "url": "/jammming/static/css/main.2db3ce2b.chunk.css"
+  },
+  {
+    "revision": "87c6de0ea578febd848f",
+    "url": "/jammming/static/js/2.dcb9c886.chunk.js"
+  },
+  {
+    "revision": "cf219bc3143fda19a3b9",
+    "url": "/jammming/static/js/main.76bf9444.chunk.js"
+  },
+  {
+    "revision": "b6268a6867a4b5c7417d",
+    "url": "/jammming/static/js/runtime-main.ddbf0872.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
